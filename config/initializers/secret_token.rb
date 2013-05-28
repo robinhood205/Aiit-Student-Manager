f79a18c5347dbb3624a49ddc96ca2ad98a4d1587
a1125ts <a1125ts@aiit.ac.jp>
2013-05-28 21:30:12 +0900

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Students::Application.config.secret_token = 'e182de2ac6ccd16c9479f338a722c65c3becff3304a7360c97aafe1f755911fd5da2a3e73b2afb0cf31d203ce7ecf261efc2b755e2116da7421c2dc61a9b75c7'
