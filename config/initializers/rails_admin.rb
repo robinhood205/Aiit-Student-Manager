9ccb2640c06146e227919b6d8d5d36e963d33d4a
a1125ts <a1125ts@aiit.ac.jp>
2013-05-31 00:20:38 +0900

    Userモデルに対してもhistry機能を有効化する2

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -17,6 +17,8 @@ RailsAdmin.config do |config|
   # If you want to track changes on your models:
   # config.audit_with :history, 'User'
   config.audit_with :history, 'Student'
+  config.audit_with :history, 'User'
+  
 
   # Or with a PaperTrail: (you need to install it first)
   # config.audit_with :paper_trail, 'User'
