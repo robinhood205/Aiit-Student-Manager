66eb3d61afd1455857eeabfbed5d55dfa7424ff2
a1125ts <a1125ts@aiit.ac.jp>
2013-06-12 18:28:46 +0900

    アソシエーションのテストを追加しました

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,27 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130612065524) do
+ActiveRecord::Schema.define(:version => 20130612091951) do
 
-  create_table "guarantors", :force => true do |t|
+  create_table "association_pbls", :force => true do |t|
     t.string   "name"
-    t.integer  "student_test_id"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.integer  "association_student_id"
+    t.datetime "created_at",             :null => false
+    t.datetime "updated_at",             :null => false
   end
 
-  add_index "guarantors", ["student_test_id"], :name => "index_guarantors_on_student_test_id"
+  create_table "association_students", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "association_tels", :force => true do |t|
+    t.string   "number"
+    t.integer  "association_student_id"
+    t.datetime "created_at",             :null => false
+    t.datetime "updated_at",             :null => false
+  end
 
   create_table "rails_admin_histories", :force => true do |t|
     t.text     "message"
@@ -102,13 +113,6 @@ ActiveRecord::Schema.define(:version => 20130612065524) do
     t.integer  "goukeiten"
     t.string   "umu"
     t.string   "hanteiriyuu"
-  end
-
-  create_table "tel_tests", :force => true do |t|
-    t.integer  "number"
-    t.integer  "student_test_id"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
   end
 
   create_table "users", :force => true do |t|
--- spec/models/student_test_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe StudentTest do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
