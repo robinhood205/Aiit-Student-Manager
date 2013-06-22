1199b5372ce66f1da8ec85cadcaff8c36c54c724
HARISAWA <rai13413jp@yahoo.co.jp>
2013-06-22 13:11:25 +0900

    Revert "Revert "a revert""
    This reverts commit 3e468b97a4ea7f346c5f9942648da9209559be87.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,58 @@
-# -*- coding: utf-8 -*-
 Students::Application.routes.draw do
-  # password(パスワード確認),registration(サインアップ)機能へのルーティングを除外
-  devise_for :users#, only: [:session]
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
 
+  # Sample of regular route:
+  #   match 'products/:id' => 'catalog#view'
+  # Keep in mind you can assign values other than :controller and :action
 
-  # RailsAdmin
-  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+  # Sample of named route:
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  # This route can be invoked with purchase_url(:id => product.id)
 
-  # ダッシュボード画面をrootに設定
-  root :to => "rails_admin/main#dashboard"
+  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
 
-  # インポート機能のroute設定
-  mount RailsAdminImport::Engine => '/rails_admin_import', :as => 'rails_admin_import'
+  # Sample resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
 
+  # Sample resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Sample resource route with more complex sub-resources
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', :on => :collection
+  #     end
+  #   end
+
+  # Sample resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => 'welcome#index'
+
+  # See how all your routes lay out with "rake routes"
+
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  # match ':controller(/:action(/:id))(.:format)'
 end
