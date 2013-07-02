04f8afa7fbc53c6524176b38f8ef79b87d31c078
HARISAWA <rai13413jp@yahoo.co.jp>
2013-07-03 02:06:03 +0900

    students_listを削除した。

--- app/models/students_list.rb
+++
@@ -1,3 +0,0 @@
-class StudentsList < ActiveRecord::Base
-  attr_accessible :E_mail, :PBL_1st, :PBL_2nd, :PBL_teacher, :class, :classification, :gakusyu_no, :grade, :name, :nationality, :nyugaku, :risyukikan, :syuryo, :user_id, :visa
-end
