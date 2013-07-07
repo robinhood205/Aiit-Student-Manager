eb07c87fda4c202d4d7c9339f08d1d7f596dff2f
Yoshihide Chubachi <yoshi@chubachi.net>
2013-07-07 17:36:29 +0900

    Add db/schema.rb to .gitignore.

--- db/schema.rb
+++ db/schema.rb
@@ -53,36 +53,6 @@ ActiveRecord::Schema.define(:version => 20130706075954) do
     t.datetime "updated_at",   :null => false
   end
 
-  create_table "privacies", :force => true do |t|
-    t.string   "name"
-    t.string   "kana_name"
-    t.string   "eng_name"
-    t.string   "gender"
-    t.date     "birthday"
-    t.string   "birthplace"
-    t.string   "zip_code"
-    t.string   "city"
-    t.string   "block"
-    t.string   "address"
-    t.string   "building"
-    t.string   "tel"
-    t.string   "mobile"
-    t.string   "pc_email"
-    t.string   "mobile_email"
-    t.string   "graduated_school"
-    t.string   "department"
-    t.string   "stream"
-    t.string   "graduated_date"
-    t.boolean  "new_graduates"
-    t.string   "occupation"
-    t.string   "company"
-    t.string   "company_address"
-    t.string   "work_period"
-    t.text     "memo"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
-  end
-
   create_table "rails_admin_histories", :force => true do |t|
     t.text     "message"
     t.string   "username"
