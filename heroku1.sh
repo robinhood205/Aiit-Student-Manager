b6e90199c459b4bc72facaaba9e1037261c2e2cc
Yoshihide Chubachi <yoshi@chubachi.net>
2013-07-17 00:46:45 +0900

    Use case instead of if

--- heroku1.sh
+++ heroku1.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 echo TRAVIS_BRANCH=${TRAVIS_BRANCH}
-if [ ${TRAVIS_BRANCH} = "master" ]; then
-    echo "it's Master!"
-elif [ ${TRAVIS_BRANCH} = "release" ]; then
-    echo "it's Release."
-else
-    echo "it's Other."
-fi
+case ${TRAVIS_BRANCH} in
+    master)
+	echo "it's Master!"
+	;;
+    release)
+	echo "it's Release."
+	;;
+    *)
+	echo "it's Other."
+	;;
+esac
