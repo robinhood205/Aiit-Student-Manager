df9df15bc77f1d5f7655e08d1f83172f755186ff
楊嘉霖 <Napoleonyjl@gmail.com>
2013-10-26 19:13:06 +0900

    enable select box

--- app/models/aiit_member.rb
+++ app/models/aiit_member.rb
@@ -1,8 +1,36 @@
 class AiitMember < ActiveRecord::Base
-  attr_accessible :birthday, :gender, :kana_name, :memo, :name
+  attr_accessible :birthday, :kana_name, :gender, :memo, :name
 
   has_one :privacy
   has_many :prospects
   has_many :exam_lists
   has_many :student_lists
+
+  rails_admin do
+  	
+	    list do
+			field :name
+			field :kana_name
+	    	field :birthday
+	      	field :gender, :enum do
+		        enum do
+		          ['Male', 'Female']
+		        end
+	     	 end
+	      	field :memo
+	    end
+
+	    edit do
+			field :name
+			field :kana_name
+	    	field :birthday
+	      	field :gender, :enum do
+		        enum do
+		          ['Male', 'Female']
+		        end
+	     	 end
+	      	field :memo
+	    end
+  end
+
 end
