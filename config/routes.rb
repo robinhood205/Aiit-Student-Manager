c8598a7db7ffd4ead6c843ab375034d49a157e94
HARISAWA <rai13413jp@yahoo.co.jp>
2013-10-26 19:16:08 +0900

    AIIT会員に「性別」の項目を選択式に変更する。

--- config/routes.rb
+++ config/routes.rb
@@ -2,33 +2,22 @@
 Students::Application.routes.draw do
 
   resources :status_records
-
-
   resources :privacies
-
-
   resources :aiit_members
-
+  get "admin/aiit_member/new" => "aiit_members#new"
 
   resources :exam_lists
-
-
   resources :tuitions
-
-
   resources :student_lists
-
-
   resources :prospects
-
   resources :guarantors
   resources :guarantors
   resources :status_records
   resources :mibuns
   resources :guarantors
+
   # password(パスワード確認),registration(サインアップ)機能へのルーティングを除外
   devise_for :users, only: [:session]
-
 
   # RailsAdmin
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
