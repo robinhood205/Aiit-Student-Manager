1a83a13171df7ca93ed28de3f9c45bb6ed3b39d5
楊嘉霖 <Napoleonyjl@gmail.com>
2013-11-01 01:40:31 +0900

    change spec to japanese

--- spec/views/prospects/index.html.haml_spec.rb
+++ spec/views/prospects/index.html.haml_spec.rb
@@ -5,7 +5,7 @@ describe "prospects/index" do
     assign(:prospects, [
       stub_model(Prospect,
         :aiit_member_id => 1,
-        :wished_program => "innovation",
+        :wished_program => "創造技術専攻",
         :wished_material => "Wished Material",
         :event => "Event",
         :contact_unnecessary => false,
@@ -14,7 +14,7 @@ describe "prospects/index" do
       ),
       stub_model(Prospect,
         :aiit_member_id => 1,
-        :wished_program => "innovation",
+        :wished_program => "創造技術専攻",
         :wished_material => "Wished Material",
         :event => "Event",
         :contact_unnecessary => false,
@@ -28,7 +28,7 @@ describe "prospects/index" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => "innovation".to_s, :count => 2
+    assert_select "tr>td", :text => "創造技術専攻".to_s, :count => 2
     assert_select "tr>td", :text => "Wished Material".to_s, :count => 2
     assert_select "tr>td", :text => "Event".to_s, :count => 2
     assert_select "tr>td", :text => "No".to_s, :count => 2
