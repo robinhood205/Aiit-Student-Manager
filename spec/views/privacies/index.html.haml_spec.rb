d0c98283fcb458faf5827e9d7091c4dc86fa1158
楊嘉霖 <Napoleonyjl@gmail.com>
2013-10-31 20:08:52 +0900

    index

--- spec/views/privacies/index.html.haml_spec.rb
+++ spec/views/privacies/index.html.haml_spec.rb
@@ -55,7 +55,6 @@ describe "privacies/index" do
   end
 
   it "renders a list of privacies" do
-    view.lookup_context.prefixes = %w[organizations application]
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => 1.to_s, :count => 2
