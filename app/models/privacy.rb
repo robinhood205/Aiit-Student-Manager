a5f3fea441899709569cec8e28a410e5956d79de
楊嘉霖 <Napoleonyjl@gmail.com>
2013-10-31 17:55:00 +0900

    add select box

--- app/models/privacy.rb
+++ app/models/privacy.rb
@@ -1,5 +1,7 @@
 class Privacy < ActiveRecord::Base
+extend Enumerize
   attr_accessible :address, :aiit_member_id, :birthplace, :block, :building, :city, :company, :company_address, :department, :eng_name, :graduated_date, :graduated_school, :memo, :mobile, :mobile_email, :new_graduates, :occupation, :pc_email, :stream, :tel, :work_period, :zip_code
 
   belongs_to :aiit_member
+  enumerize :stream, in: [:literature, :science, :else]
 end
