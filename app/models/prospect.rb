b06b98c23d7a688afa5612f50d60ccb2b4c9c7a1
楊嘉霖 <Napoleonyjl@gmail.com>
2013-10-31 18:03:06 +0900

    add select box

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -1,5 +1,7 @@
 class Prospect < ActiveRecord::Base
+extend Enumerize
   attr_accessible :aiit_member_id, :applikation, :contact_unnecessary, :event, :memo, :received_date, :visited_date, :wished_material, :wished_program
 
   belongs_to :aiit_member
+  enumerize :wished_program, in: [:innovation, :information, :unknown]
 end
