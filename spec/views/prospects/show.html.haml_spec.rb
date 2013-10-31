1d52ff25b32c4f5bf733a34c7ffdf36d0a369bb9
楊嘉霖 <Napoleonyjl@gmail.com>
2013-11-01 06:00:23 +0900

    fix

--- spec/views/prospects/show.html.haml_spec.rb
+++ spec/views/prospects/show.html.haml_spec.rb
@@ -1,10 +1,11 @@
+# coding: utf-8
 require 'spec_helper'
 
 describe "prospects/show" do
   before(:each) do
     @prospect = assign(:prospect, stub_model(Prospect,
       :aiit_member_id => 1,
-      :wished_program => "innovation",
+      :wished_program => "創造技術専攻",
       :wished_material => "Wished Material",
       :event => "Event",
       :contact_unnecessary => false,
@@ -17,7 +18,7 @@ describe "prospects/show" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/1/)
-    rendered.should match(/innovation/)
+    rendered.should match(/創造技術専攻/)
     rendered.should match(/Wished Material/)
     rendered.should match(/Event/)
     rendered.should match(/false/)
