120de416b63ced6e2ed6f1b33a404c8234c34862
楊嘉霖 <Napoleonyjl@gmail.com>
2013-11-01 01:19:12 +0900

    complete

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -4,5 +4,5 @@ extend Enumerize
   attr_accessible :aiit_member_id, :applikation, :contact_unnecessary, :event, :memo, :received_date, :visited_date, :wished_material, :wished_program
 
   belongs_to :aiit_member
-  enumerize :wished_program, in: [:創造, :情報, :不明]
+  enumerize :wished_program, in: [:創造技術専攻, :情報アーキテクチャ専攻, :不明]
 end
