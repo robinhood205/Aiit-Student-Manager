e00f70b6339d3e971ce33754185c1cc95e6a340a
楊嘉霖 <Napoleonyjl@gmail.com>
2013-11-01 01:12:44 +0900

    change to japanese

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -1,7 +1,8 @@
+# coding: utf-8
 class Prospect < ActiveRecord::Base
 extend Enumerize
   attr_accessible :aiit_member_id, :applikation, :contact_unnecessary, :event, :memo, :received_date, :visited_date, :wished_material, :wished_program
 
   belongs_to :aiit_member
-  enumerize :wished_program, in: [:innovation, :information, :unknown]
+  enumerize :wished_program, in: [:創造, :情報, :不明]
 end
