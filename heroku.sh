acb78bce68654d1e037dd2de530be56b1e6a477b
ぎゅう <a1217yn@aiit.ac.jp>
2013-11-07 17:34:11 +0900

    Update heroku.sh
    release

--- heroku.sh
+++ heroku.sh
@@ -4,7 +4,7 @@ case ${TRAVIS_BRANCH} in
     master)
 	echo "it's Master!"
    	wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-	git remote add heroku git@heroku.com:studentsmanager2.git
+	git remote add heroku git@heroku.com:aiitstudents.git
 	echo "Host heroku.com" >> ~/.ssh/config
 	echo "   StrictHostKeyChecking no" >> ~/.ssh/config
 	echo "   CheckHostIP no" >> ~/.ssh/config
@@ -12,7 +12,7 @@ case ${TRAVIS_BRANCH} in
 	heroku keys:clear
 	yes | heroku keys:add
 	yes | git push -f heroku master
-	heroku pg:reset DATABASE --confirm studentsmanager2
+	heroku pg:reset DATABASE --confirm aiitstudents
 	heroku run rake db:migrate
 	heroku run rake db:seed
 	heroku restart  
@@ -21,7 +21,7 @@ case ${TRAVIS_BRANCH} in
     release)
 	echo "it's Release."
 	wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-	git remote add heroku git@heroku.com:studentsmanager.git
+	git remote add heroku git@heroku.com:aiitstudentstest.git
 	echo "Host heroku.com" >> ~/.ssh/config
 	echo "   StrictHostKeyChecking no" >> ~/.ssh/config
 	echo "   CheckHostIP no" >> ~/.ssh/config
