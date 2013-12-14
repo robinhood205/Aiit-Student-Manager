cccd1d7a5eb25672e6e5bcf3d9b86be15f66ff4b
n774 <hoge@example.com>
2013-12-14 16:19:58 +0900

    add validation

--- app/models/status_record.rb
+++ app/models/status_record.rb
@@ -7,4 +7,6 @@ extend Enumerize
 
   enumerize :absence_reason, in: [:業務都合, :経済的理由, :家庭の事情, :健康上の理由, :その他]
   enumerize :absence_time, in: [:退学, :一年, :半年, :その他]
+
+  validates_presence_of :student_list_id
 end
