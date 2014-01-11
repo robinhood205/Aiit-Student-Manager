033a31df7efa43d8434db01a662c8ebae4f95c5e
HARISAWA <rai13413jp@yahoo.co.jp>
2014-01-11 15:03:26 +0900

    身分異動の項目を編集した。

--- spec/views/status_records/show.html.haml_spec.rb
+++ spec/views/status_records/show.html.haml_spec.rb
@@ -5,7 +5,7 @@ describe "status_records/show" do
   before(:each) do
     @status_record = assign(:status_record, stub_model(StatusRecord,
       :student_list_id => 1,
-      :status => "Status",
+      :status => "退学",
       :absence_reason => "業務都合",
       :absence_time => "一年",
       :return_reason => "経済的理由",
@@ -17,7 +17,7 @@ describe "status_records/show" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/1/)
-    rendered.should match(/Status/)
+    rendered.should match(/退学/)
     rendered.should match(/業務都合/)
     rendered.should match(/一年/)
     rendered.should match(/経済的理由/)
